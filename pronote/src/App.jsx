886164917ab4cc04f2a63fd7f72055184884a30f
Torsha-biswas <torshabiswas19@gmail.com>
2024-05-14 23:32:57 +0530

    done

---
+++ pronote/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Notepad from './components/Editor';
+
+function App() {
+  return (
+    <div className="App">
+    <Notepad />
+ 
+    </div>
+  );
+}
+
+export default App;
